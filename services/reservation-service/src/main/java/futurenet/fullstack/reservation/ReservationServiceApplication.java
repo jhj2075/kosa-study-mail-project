58d26f10889eb75e14e8e957fbf22abbc2811bee
jhj2075 <flg989@naver.com>
2026-06-15 00:18:14 +0900

    feat: 예약 메일 CRUD 구현

--- services/reservation-service/src/main/java/futurenet/fullstack/reservation/ReservationServiceApplication.java
+++ services/reservation-service/src/main/java/futurenet/fullstack/reservation/ReservationServiceApplication.java
@@ -1,12 +1,14 @@
 package futurenet.fullstack.reservation;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("futurenet.fullstack.reservation.mapper")
 @SpringBootApplication
 public class ReservationServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ReservationServiceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ReservationServiceApplication.class, args);
+  }
 }
